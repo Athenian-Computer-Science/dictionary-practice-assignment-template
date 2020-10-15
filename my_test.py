2d5c38687cffa99b4c8ec0882aa41a932758aae3
Megan Leich <51799170+maleich@users.noreply.github.com>
2020-10-14 21:32:16 -0700

    create my_code, my_test

--- my_test.py
+++ my_test.py
@@ -1,7 +1,17 @@
-from my_code import inc
+from my_code import *
 
 
-def test_inc():
-    assert 5 == inc(4)
-    assert 1 == inc(0)
-    assert 0 == inc(-1)
+def test_make_dict():
+    assert {'Ten': 10, 'Twenty': 20, 'Thirty': 30} == make_dict()
+
+def test_add_element():
+    assert {'fruit': 'apple', 'veggie': 'carrot', 'grain': 'barley', 'dairy': 'yogurt'} == add_element()
+
+def test_remove_element():
+    assert {'fruit': 'apple', 'grain': 'barley'} == remove_element()
+
+def test_merge_dict():
+    assert {'Ten': 10, 'Twenty': 20, 'Thirty': 30, 'Fourty': 40, 'Fifty': 50} == merge_dict()
+
+def test_access_key():
+    assert 20 == access_key()
